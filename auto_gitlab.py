e6d13f58c114a06d91252742d0bc33161538b6eb
mlamamra <mlamamra@dawan.fr>
2022-10-28 16:43:06 +0200

    test génération clés

--- auto_gitlab.py
+++ auto_gitlab.py
@@ -25,8 +25,13 @@ if not gl.projects.list(search=GITLAB_PROJECT):
     print(f"projet {GITLAB_PROJECT} created !!")
 
 
-ssh_path = f"c:/Users/{os.getlogin()}/.ssh"
+ssh_path = f"c:/Users/{os.getlogin()}.DESKTOP-8967908/.ssh"
 # appel à ssh-keygen.exe à travers python (system call)
 # 1. découpe de la commande dans une liste
 # 2. utilisation du paramètre input pour anticiper les demandes du script sur stdin
-subprocess.run(["ssh-keygen", "-f", f"{ssh_path}/{GITLAB_PROJECT}"], input="\n\n")
+# investiguer sous windows
+
+# tester l'existence de la clé avant
+if not os.path.exists(f"{ssh_path}/{GITLAB_PROJECT}"):
+    subprocess.run(["ssh-keygen", "-f", f"{ssh_path}/{GITLAB_PROJECT}"], input=b'\r\n\r\n')
+    print("pubkey/privkey generated !!")
